5e8c12d8440abcf2349dbe900015bc4a6c909f95
Caike Souza <carloshrsouza@gmail.com>
2013-02-19 19:00:23 -0500

    Fix issues on FB, going back to specific verbs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 OscarBolao::Application.routes.draw do
-  match 'auth/:provider/callback', to: 'sessions#create', via: :any
-  match 'logout', to: 'sessions#destroy', as: 'logout', via: :any
-  match 'auth/failure', to: redirect('/'), via: :any, via: :any
+  get 'auth/:provider/callback', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  match 'auth/failure', to: redirect('/'), via: :any
 
   resources :votes
   root to: 'entry#new'
